867ccd7fa07a074dc22da8149a3875cbdd9ea010
Germano Silva <104945531+Germano-Silva@users.noreply.github.com>
2025-01-12 01:11:53 -0300

    Add examples of do-while and while loops

---
+++ Fundamentos-Java/src/aula_16/DoWhile_01.java
@@ -0,0 +1,16 @@
+package aula_16;
+
+public class DoWhile_01 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		do {
+			i++;
+			System.out.println("Valor de i " + i);
+			
+		}while(i<11);
+		System.out.println(i);
+
+	}
+
+}
---
+++ Fundamentos-Java/src/aula_16/While_01.java
@@ -0,0 +1,21 @@
+package aula_16;
+
+public class While_01 {
+
+	public static void main(String[] args) {
+		int i = 1; //variavel contador count ou cont
+		int max = 10;
+		
+		System.out.println("Conte até "+max);
+		
+		while (i<=max) {
+			System.out.println("O valor de i:" + i);
+			i++;
+			
+			
+		}
+		System.out.println(i);
+
+	}
+
+}
